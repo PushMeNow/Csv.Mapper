53a19d5393cb56dbb202d2baa1546b15a1944071
Konstantin Borovik <kes8594@yandex.ru>
2021-12-19 21:48:42 +0300

    - initial commit

---
+++ Csv.Mapper.Attributes/FieldAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Dawn;
+
+namespace Csv.Mapper.Attributes
+{
+    public class FieldAttribute : Attribute
+    {
+        private readonly int _columnIndex;
+
+        public FieldAttribute(int columnIndex)
+        {
+            Guard.Argument(_columnIndex, nameof(columnIndex))
+                 .NotNegative();
+            
+            _columnIndex = columnIndex;
+        }
+    }
+}
